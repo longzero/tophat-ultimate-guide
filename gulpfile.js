2638d68b36f2e4937858fdbb22fef665ba76e3b7
Long Nguyen <lnguyen@globalia.ca>
2020-08-01 12:50:21 -0400

    setup: using gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
-var gulpCssPreprocessor = require('gulp-css-preprocessor');
+// var gulpCssPreprocessor = require('gulp-css-preprocessor');
 var header = require('gulp-header');
 var minify = require('gulp-minifier');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
 
 // CSS preprocessor
 gulp.task('styles', function() {
   return gulp.src('src/css/styles.scss')
-    .pipe(gulpCssPreprocessor())
+    .pipe(sass().on('error', sass.logError))
     .pipe(header('/* Generated on: ' + new Date() + ' */\n'))
     .pipe(gulp.dest('public/css/'))
     .pipe(minify({minify: true, minifyCSS: true}))
